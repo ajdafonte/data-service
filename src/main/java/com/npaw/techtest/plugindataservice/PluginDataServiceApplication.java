f99d1244be94893715f30014d5b7129a98b1eb83
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2019-04-04 17:35:11 +0200

    Initial setup

---
+++ src/main/java/com/npaw/techtest/plugindataservice/PluginDataServiceApplication.java
@@ -0,0 +1,16 @@
+package com.npaw.techtest.plugindataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PluginDataServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PluginDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/npaw/techtest/plugindataservice/PluginDataServiceApplicationTests.java
@@ -0,0 +1,19 @@
+package com.npaw.techtest.plugindataservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class PluginDataServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
